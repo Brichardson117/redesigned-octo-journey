e53a867e2c0d937793018e2eb911e9d46eddd8f8
Brittney <brittneyrichardson8@gmail.com>
2022-09-12 17:33:22 -0500

    updated script and html

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let searchBtn = document.querySelector("#btn");
 let cityNameEl = document.querySelector("#state");
 let cityName = " ";
 
+//function to get user input
 let searchLocationHandler = function (event) {
   event.preventDefault();
 
@@ -43,28 +44,42 @@ function apiCall() {
     });
 }
 
+//function to display weather information for current weather
 function displayWeather(data, name) {
+    //create element for city name and assign text content and class element
     let cityName = document.createElement('h2');
     cityName.textContent = name;
     cityName.classList = 'uk-card-title'
+
+    //create element for temperate and assign text content and class element
     let temperature = document.createElement('p');
     temperature.textContent = `Temp: ${data.current.temp} °F`;
     temperature.classList = 'uk-card-body'
+
+    //create element for weather imageand assign text content and class element
     let weatherImg = document.createElement('img');
     weatherImg.src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`
     weatherImg.classList = 'uk-card-media-top'
+
+    //create element for humidity and assign text content and class element
     let humidity = document.createElement('p')
     humidity.textContent = `Humidity: ${data.current.humidity}%`
     humidity.classList = 'uk-card-body'
+
+    //create element for uv and assign text content and class element
     let uv = document.createElement('p')
     uv.textContent = `Uv Index: ${data.current.uvi}`;
     uv.classList = 'uk-card-body'
+
+    //create element for wind speen and assign text content and class element
     let windSpeed = document.createElement('p');
     windSpeed.textContent = `Windspeed: ${data.current.wind_speed}`
     windSpeed.classList = 'uk-card-body'
+
+    //append created elements to html 
     document.querySelector('#displayWeather').append(weatherImg, cityName, temperature, humidity, uv, windSpeed)
 }
 
 
-
+//when user clicks find button, run apiCall function
 searchBtn.addEventListener("click", apiCall);
